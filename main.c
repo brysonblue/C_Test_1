5d07f51a05f33264b79d7fca95a86e743ef748fd
brysonblue <128836043+brysonblue@users.noreply.github.com>
2024-07-21 08:33:07 -0400

    Added comments.

--- main.c
+++ main.c
@@ -6,6 +6,12 @@ int main(void)
     //this is a comment
     /* This is a multiline comment
      */
+    /* escape sequence = character combination consisting of a backslash \
+     *                   followed by a letter or combination of digits.
+     *                   they specify actions within a line or string of text.
+     *                   \n = newline
+     *                   \t = tab
+     */
     printf("Hello, World!\n");
     return 0;
 }
