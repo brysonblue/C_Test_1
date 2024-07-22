72f06c4252ff638a3d3399db745246edd682783b
brysonblue <128836043+brysonblue@users.noreply.github.com>
2024-07-22 15:45:45 -0400

    Added format specifiers.

--- main.c
+++ main.c
@@ -75,5 +75,30 @@ int main(void)
     //printf("%lld\n", l);  // long long int
     //printf("%llu\n", m);  // unsigned long long int
 
+
+    //   format specifier % = defines and formats a type of data to be displayed
+
+    // %c = character
+    // %s = string (array of characters)
+    // %f = float
+    // %lf = double
+    // %d = integer
+
+    // %.1 = decimal precision
+    // %1 = minimum field width
+    // %- = left align
+
+    float item1 = 5.75;
+    float item2 = 10.00;
+    float item3 = 100.99;
+
+    printf("Item 1: $%f\n", item1);
+    printf("Item 1: $%.2f\n", item2);
+    printf("Item 1: $%f8\n", item3);
+    printf("Item 1: $%-f\n", item1);
+    printf("Item 1: $%f\n", item1);
+
+
+
     return 0;
 }
