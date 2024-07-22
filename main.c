36e7c5ec1b56f7b248ec9904d4ebb6d824945130
brysonblue <128836043+brysonblue@users.noreply.github.com>
2024-07-22 15:33:57 -0400

    Added a lot of comments and notes.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
@@ -34,13 +35,45 @@ int main(void)
     char grade = 'A'; //single character
     char name[] = "Bro"; //array of characters
 
+
     printf("Hello %s\n", name);
     printf("You are %d years old\n", age);
     printf("Your average grade is %c\n", grade);
     printf("Your GPA is %f\n", gpa);
 
+    char a = 'C';                // single character    %c
+    char b[] = "Bro";            // array of characters %s
 
+    float c = 3.141592;          // 4 bytes (32 bits of precision) 6 - 7 digits %f
+    double d = 3.141592653589793;// 8 bytes (64 bits of precision) 15 - 16 digits %lf
 
+    bool w =  true;              // 1 byte (true or false) %d
+
+    //char f = 100;              // 1 byte (-128 to +127) %d or %c
+    //unsigned char g = 255;     // 1 byte (0 to +255) %d or %c
+
+    //short int h = 32767;       // 2 bytes (-32,768 to +32,767) %d
+    //unsigned short int i = 65535; // 2 bytes (0 to +65,535) %d
+
+    int j = 2147483647;          // 4 bytes (-2,147,483,648 to +2,147,483,647) %d
+    //unisgned int k = 4294967295L; // 4 bytes (0 to +4,294,967,295) %u
+
+    //long long int 1 = 9223372036854775807;           // 8 bytes (-9 quintillion to +9 quintillion) %lld
+    //unisnged long long int m = 1844674407370955161U; // 8 bytes (0 to +18 quintillion) %llu
+
+    //printf("%c\n", a);    // char
+    //printf("%s\n", b);    // character array
+    //printf("%f\n", c);    // float
+    //printf("%lf\n", d);   // double
+    //printf("%d\n", e);    // bool
+    //printf("%d\n", f);    // char as numeric value
+    //printf("%d\n", g);    // unsigned char as numeric value
+    //printf("%d\n", h);    // short
+    //printf("%d\n", i);    // unisgned short
+    //printf("%d\n", j);    // int
+    //printf("%u\n", k);    // unsigned int
+    //printf("%lld\n", l);  // long long int
+    //printf("%llu\n", m);  // unsigned long long int
 
 
 
