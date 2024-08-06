4d1e5d52b3c845bd2531af5909c3a590dd277b52
brysonblue <128836043+brysonblue@users.noreply.github.com>
2024-08-06 08:41:27 -0400

    Added format specifiers.

--- main.c
+++ main.c
@@ -165,5 +165,35 @@ int main(void)
     printf("How old are you?");
     scanf("%d", age);
     */
+
+    /*
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
+
+
+    int main(void)
+    {
+
+        char name[25]; //bytes
+        int age;
+
+        printf("\nWhat's your name?");
+        //scanf("%s", &name);                        // scanf is an input function up to a white space
+        fgets(name, 25, stdin);             /* fgets function is an input that reads white spaces,
+                                                        stdin is standard input * /
+        name[strlen(name)-1] = '\0';                // strlen == string length
+
+        printf("\nHow old are you?");
+        scanf("%d", &age);
+
+        printf("\nHello %s, how are you?", name);
+        printf("\nYou are %d years old", age);
+
+        return 0;
+    }
+    */
+
     return 0;
 }
+
